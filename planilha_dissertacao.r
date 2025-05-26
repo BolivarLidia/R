c12ae4f88e61910582c2b35fac1d19152411e5a0
bolivar <bolivarlidia@gmail.com>
2025-05-26 10:30:02 -0300

    adicionando comentário

--- planilha_dissertacao.r
+++ planilha_dissertacao.r
@@ -1,3 +1,4 @@
 install.packages("gmodels")
 X_planilha_dissertação_resultados_e_variáveis_version_1_ <- read_excel("Pasta do R/~$planilha dissertação - resultados e variáveis (version 1).xlsx")
 X_planilha_dissertação_resultados_e_variáveis <- read_excel("Pasta do R/~$planilha dissertação - resultados e variáveis (version 1).xlsx")
+#test
